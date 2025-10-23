5448e8d4c2bb037e146fc093113645427872435f
Sungrae Hong <s-hong.1@onu.edu>
2025-10-23 12:19:35 -0400

    Completed countVowel function.

--- p8-1/p8-1.cpp
+++ p8-1/p8-1.cpp
@@ -3,15 +3,23 @@
 
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main() {
 	string str = "Hello, World!";
-	cout << countCharacter(str) << endl;
+	cout << countVowel(str) << endl;
 
 	return 0;
 }
 
-int countCharacter(string str) {
-	return str.length();
+int countVowel(string str) {
+	int result = 0;
+	for (char c : str) {
+		c = tolower(c);
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+			result++;
+		}
+	}
+
+	return result;
 }
